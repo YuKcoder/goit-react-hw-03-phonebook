769f5772f37671c44c281ed58eef1f562f52e836
Yurii Kovtunenko <kovtunenko.91.09@gmail.com>
2022-01-25 16:32:33 +0200

    Adds contact storage in local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,14 +9,20 @@ import Filter from '../Filter';
 
 export default class Add extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    contacts && this.setState({ contacts });
+  }
 
   checkContact = name => {
     const { contacts } = this.state;
